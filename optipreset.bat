a534251dcb181262bc6454792ef17f0ac9679c04
RTX <98684905+RTX4O9O@users.noreply.github.com>
2024-07-10 01:50:09 +0800

    Update optipreset.bat

--- optipreset.bat
+++ optipreset.bat
@@ -1,5 +1,5 @@
 @echo off
-powershell -Command "Invoke-WebRequest https://cdn.discordapp.com/attachments/1115180823038148649/1115183133004922890/optionsof.txt -Outfile optionsof.txt"
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/RTX4O9O/Optipreset/main/optionsof.txt -Outfile optionsof.txt"
 cls
 set /p path= "Please enter your .minecraft path: "
 cls
